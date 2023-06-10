54f89ef6d500e98d97eb61f7debb86aab65342c8
MapanKemal <kemal.mapan@gmail.com>
2023-06-10 12:09:24 +0700

    Update: CreateEmployee done

--- app/Http/Controllers/Master/Employee.php
+++ app/Http/Controllers/Master/Employee.php
@@ -8,6 +8,10 @@ use App\Models\Master\EmployeePosition;
 use App\Models\User;
 use Haruncpi\LaravelIdGenerator\IdGenerator;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\Rule;
 
 class Employee extends Controller
 {
@@ -20,47 +24,83 @@ class Employee extends Controller
     }
     public function create_Employee(Request $request)
     {
-        /* Create User  */
-        $user = User::create([
-            "ALIASES" => $request->aliases,
-            "USERNAME" => $request->username,
-            "PASSWORD" => $request->password,
-            "EMAIL" => $request->email,
-        ]);
-        /* Create Employee  */
-        $empNumbConf = [
-            'table' => 'ms_employee',
-            'field' => 'EMPL_NUMBER',
-            'length' => 16,
-            'prefix' =>  "EMPL-" . date("Ymd")
-        ];;
-        $empNumber = IdGenerator::generate($empNumbConf);
-        $employee = MasterEmployee::create([
-            "EMPL_NUMBER" => $empNumber,
-            "USER_ID" => $user->USER_ID,
-            "EMPL_UNIQUE_CODE" => $request->eployeeNumber,
-            "EMPL_FIRSTNAME" => $request->firstName,
-            "EMPL_LASTNAME" => $request->lastName,
-            "EMPL_GENDER" => $request->gender
-        ]);
-        /* Create FR-Employee  */
-        $employeePosition = EmployeePosition::create([
-            "USER_ID" => $user->USER_ID,
-            "EMPL_ID" => $employee->EMPL_ID,
-            "COMP_ID" => $request->company,
-            "DEPT_ID" => $request->departement,
-            "POST_ID" => $request->position,
-            "ACCP_LOWER" => 0,
-            "ACCP_LOWER_AMOUNT" => 0,
-            "ACCP_UPPER" => 0,
-            "ACCP_UPPER_AMOUNT" => 0,
-            "ON_STRUCTURE" => 0,
-        ]);
-        return response([
-            "user" => $user,
-            "employee" => $employee,
-            "position" => $employeePosition,
-        ]);
+        // Validation
+        $validateUser = Validator::make(
+            $request->all(),
+            [
+                'username' => ['required', Rule::unique('ms_users', 'USERNAME'), 'min:2'],
+                'password' => ['required', 'min:3', 'max:10'],
+                'eployeeNumber' => ['required', Rule::unique('ms_employee', 'EMPL_UNIQUE_CODE'), 'min:1'],
+                'company' => ['required'],
+                'departement' => ['required'],
+                'position' => ['required'],
+            ]
+        );
+
+        if ($validateUser->fails()) {
+            return response([
+                'status' => false,
+                'message' => 'User Validation Error',
+                'errors' => $validateUser->errors()
+            ], 400);
+        }
+
+        try {
+            DB::beginTransaction();
+            /* Create User  */
+            $user = User::create([
+                "ALIASES" => $request->aliases,
+                "USERNAME" => $request->username,
+                "PASSWORD" => Hash::make($request->password),
+                "EMAIL" => $request->email,
+            ]);
+            /* Create Employee  */
+            $empNumbConf = [
+                'table' => 'ms_employee',
+                'field' => 'EMPL_NUMBER',
+                'length' => 16,
+                'prefix' =>  "EMPL-" . date("Ymd"),
+                'reset_on_prefix_change' => true,
+
+            ];
+            $empNumber = IdGenerator::generate($empNumbConf);
+            $employee = MasterEmployee::create([
+                "EMPL_NUMBER" => $empNumber,
+                "USER_ID" => $user->USER_ID,
+                "EMPL_UNIQUE_CODE" => $request->eployeeNumber,
+                "EMPL_FIRSTNAME" => $request->firstName,
+                "EMPL_LASTNAME" => $request->lastName,
+                "EMPL_GENDER" => $request->gender
+            ]);
+            /* Create FR-Employee  */
+            $employeePosition = EmployeePosition::create([
+                "USER_ID" => $user->USER_ID,
+                "EMPL_ID" => $employee->EMPL_ID,
+                "COMP_ID" => $request->company,
+                "DEPT_ID" => $request->departement,
+                "POST_ID" => $request->position,
+                "ACCP_LOWER" => 0,
+                "ACCP_LOWER_AMOUNT" => 0,
+                "ACCP_UPPER" => 0,
+                "ACCP_UPPER_AMOUNT" => 0,
+                "ON_STRUCTURE" => 0,
+            ]);
+
+            /* Return Response on success */
+            return response([
+                "user" => $user,
+                "employee" => $employee,
+                "position" => $employeePosition,
+            ], 200);
+            DB::commit();
+        } catch (\Throwable $e) {
+            DB::rollBack();
+            /* Return Response on error */
+            return response([
+                "error" => $e->getMessage(),
+                "message" => "Sorry, System can't receive your request",
+            ], 500);
+        }
     }
     public function update_Employee()
     {
